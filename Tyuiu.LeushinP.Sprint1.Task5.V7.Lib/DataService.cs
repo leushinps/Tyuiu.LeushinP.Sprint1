081223ede6dc59f18f9113b3612456337237cab9
Pavel Leushin <leushinps@outlook.com>
2025-10-20 03:27:38 +0500

    Task5

--- Tyuiu.LeushinP.Sprint1.Task5.V7.Lib/DataService.cs
+++ Tyuiu.LeushinP.Sprint1.Task5.V7.Lib/DataService.cs
@@ -7,26 +7,12 @@ namespace Tyuiu.LeushinP.Sprint1.Task5.V7.Lib
     {
         public int AngleToHoursMinutes(double x)
         {
-            if (x <= 0 || x >= 360)
-                throw new ArgumentException("Угол должен быть в диапазоне: 0 < x < 360");
-
+            if (x < 0 || x >= 360)
+                throw new ArgumentException("Угол должен быть в диапазоне от 0 до 360");
             double totalHours = x / 30.0;
-
-
-            return (int)totalHours;
-        }
-
-        public (int hours, int minutes, int seconds) CalculateTimeComponents(double x)
-        {
-            double totalHours = x / 30.0;
-
             int hours = (int)totalHours;
-            double fractionalHours = totalHours - hours;
-            int minutes = (int)(fractionalHours * 60);
-            double fractionalMinutes = fractionalHours * 60 - minutes;
-            int seconds = (int)(fractionalMinutes * 60);
 
-            return (hours, minutes, seconds);
+            return hours;
         }
     }
 }
