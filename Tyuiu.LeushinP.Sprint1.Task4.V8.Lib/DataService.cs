6e8436346377614ba95dd18c4518de1eafa3e14c
Pavel Leushin <leushinps@outlook.com>
2025-10-26 03:00:39 +0500

    Task4

--- Tyuiu.LeushinP.Sprint1.Task4.V8.Lib/DataService.cs
+++ Tyuiu.LeushinP.Sprint1.Task4.V8.Lib/DataService.cs
@@ -8,10 +8,10 @@ namespace Tyuiu.LeushinP.Sprint1.Task4.V8.Lib
     {
         public double Calculate(double x, double y)
         {
-            double numerator = Math.Sqrt(x - y) + 3 * x;
-            double denominator = 3 + Math.Abs(x);
+            double num = Math.Sqrt(x - y) + 3 * x;
+            double den = 3 + Math.Abs(x);
 
-            return numerator / denominator;
+            return num / den;
         }
     }
 }
