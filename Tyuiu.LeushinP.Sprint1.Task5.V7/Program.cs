21a6710c98118acc58a6239099d380db3610212c
Pavel Leushin <leushinps@outlook.com>
2025-11-26 23:26:29 +0500

    Task5

---
+++ Tyuiu.LeushinP.Sprint1.Task5.V7/Program.cs
@@ -0,0 +1,37 @@
+﻿using Tyuiu.LeushinP.Sprint1.Task5.V7.Lib;
+namespace Tyuiu.LeushinP.Sprint1.Task5.V7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Леушин П.С | СМАРТб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Преобразование типов и класс Convert                              *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #7                                                              *");
+            Console.WriteLine("* Выполнил: Леушин Павел Сергеевич | СМАРТб-25-1                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Определить h – полное количество часов прошедших от начала суток до     *");
+            Console.WriteLine("* того момента (в первой половине дня), когда часовая стрелка повернулась *");
+            Console.WriteLine("* на f градусов (0<f<360, f – вещественное число).                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            double f;
+
+            Console.WriteLine("Введите значение f:");
+            f = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.AngleToHoursMinutes(f));
+            Console.ReadKey();
+        }
+    }
+}
