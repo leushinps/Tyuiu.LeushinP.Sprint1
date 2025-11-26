3556b42d6619fd9d0f61a3f63cd24ce3d24cef2a
Pavel Leushin <leushinps@outlook.com>
2025-11-26 22:47:16 +0500

    Task0

--- Tyuiu.LeushinP.Sprint.1.Task0.V26.Lib/DataService.cs
+++ Tyuiu.LeushinP.Sprint.1.Task0.V26.Lib/DataService.cs
@@ -4,7 +4,7 @@ namespace Tyuiu.LeushinP.Sprint1.Task0.V26.Lib
 
 {
     public class DataService : ISprint1Task0V26
-    {
+    { 
         public double Calculate()
         {
             return 4 * 5 / 2 - 18 / 2 / 3;
