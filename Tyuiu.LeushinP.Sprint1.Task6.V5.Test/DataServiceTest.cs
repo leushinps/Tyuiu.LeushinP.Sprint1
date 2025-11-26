72a3e3450899b637b9c4ab2fded13052006acbaa
Pavel Leushin <leushinps@outlook.com>
2025-11-26 23:29:46 +0500

    Task5

---
+++ Tyuiu.LeushinP.Sprint1.Task6.V5.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+namespace Tyuiu.LeushinP.Sprint1.Task6.V5.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
